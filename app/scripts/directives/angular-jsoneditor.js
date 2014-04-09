c43f921af12646aa4e94ed79f05c62951d12795b
slopjong <git@slopjong.de>
2014-04-09 17:20:07 +0200

    Added a drag directive (because know-how lacks of how to do it in angular directive)

--- app/scripts/directives/angular-jsoneditor.js
+++ app/scripts/directives/angular-jsoneditor.js
@@ -5,10 +5,18 @@ angular.module("jsoneditor", ['ui.ace'])
   .directive("jeSplitter", ['$timeout', function($timeout) {
     return {
       restrict: 'EA',
-      template: '<div class="je-splitter" ng-transclude></div>',
+//      template: '<div class="je-splitter" ng-transclude></div>',
+      template: function(a, b, c) {
+        var el = b.$$element;
+        var el2 = angular.element(el[0]);
+        //console.log('template', el2);
+
+//        console.log(arguments);
+        return '<div class="je-splitter" ng-transclude></div>';
+      },
       replace: true,
       transclude: true,
-      controller: function jeContainerController($scope, $element, $attrs, $transclude) {
+      controller: function jeSplitterController($scope, $element, $attrs, $transclude) {
         $scope.jsoneditor = {
           json: '{"test": "tasdfasdf"}',
           ace: {
@@ -21,9 +29,42 @@ angular.module("jsoneditor", ['ui.ace'])
             }
           }
         };
+
+        // TODO: the additional containers are hidden by css
+        //       this should be done here but there was trouble to access
+        //       $$element properly to remove the containers from the dom
+        this.addSplitter = function() {
+//          console.log('add splitter', arguments);
+        }
+
+        // TODO: the additional containers are hidden by css
+        //       this should be done here but there was trouble to access
+        //       $$element properly to remove the containers from the dom
+        this.removeContainer = function(element) {
+//          console.log(element, element[0]);
+//          console.log('remove container', arguments);
+        }
       },
-      link: function(scope, iElement, iAttr) {},
-      compile: function(scope, iElement, iAttr) {}
+      link: function(scope, element, attrs, ctrl) {
+        ctrl.removeContainer(element);
+        ctrl.addSplitter(element);
+      }
+
+//      compile: function(scope, iElement, iAttr) {
+////        angular.forEach(arguments, function(element){
+////          console.log(element);
+////        })
+//        console.log('compile je-splitter', arguments);
+//        return {
+//          pre: function preLink() {
+//            console.log('prelink je-splitter', arguments);
+//          },
+//          post: function postLink() {
+//            console.log('postlink je-splitter', iElement);
+//
+//          }
+//        }
+//      }
     };
   }])
 
@@ -33,8 +74,22 @@ angular.module("jsoneditor", ['ui.ace'])
       template: '<div class="je-container" ng-transclude></div>',
       replace: true,
       transclude: true,
-      link: function($scope, iElement, iAttr) {},
-      compile: function(scope, iElement, iAttr) {}
+      link: function($scope, iElement, iAttr) {
+//        console.log('link je-container');
+//        console.log($scope.$parent);
+      }
+    };
+  })
+
+  .directive("jeDrag", function() {
+    return {
+      restrict: 'EA',
+      template: '<div class="je-drag">⋮</div>',
+      replace: true,
+      link: function($scope, iElement, iAttr) {
+//        console.log('link je-container');
+//        console.log($scope.$parent);
+      }
     };
   })
 
@@ -44,8 +99,8 @@ angular.module("jsoneditor", ['ui.ace'])
       template: '<div class="je-text"><textarea ng-model="$parent.jsoneditor.json"></textarea></div>',
       replace: true,
       link: function(scope, iElement, iAttr) {
-      },
-      compile: function(scope, iElement, iAttr) {}
+//        console.log('link je-text');
+      }
     };
   })
 
@@ -55,8 +110,8 @@ angular.module("jsoneditor", ['ui.ace'])
       template: '<div class="je-ace" ui-ace="$parent.jsoneditor.ace.options" ng-model="$parent.jsoneditor.json"></div>',
       replace: true,
       transclude: true,
-      link: function(scope, iElement, iAttr) { },
-      compile: function(scope, iElement, iAttr) {
+      link: function(scope, iElement, iAttr) {
+//        console.log('link je-ace');
       }
     };
   })
@@ -67,8 +122,9 @@ angular.module("jsoneditor", ['ui.ace'])
       template: '<div class="je-tree"><textarea ng-model="$parent.jsoneditor.json"></textarea></div>',
       replace: true,
       transclude: true,
-      link: function(scope, iElement, iAttr) {},
-      compile: function(scope, iElement, iAttr) {}
+      link: function(scope, iElement, iAttr) {
+//        console.log('link je-tree');
+      }
     };
   })
   ;
