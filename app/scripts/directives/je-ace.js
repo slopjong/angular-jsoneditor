1791a4d9afd6f8557e4679bc324d8178d1cd35fd
slopjong <git@slopjong.de>
2014-04-26 17:34:43 +0200

    Renamed the link function arguments of the je-ace directive

--- app/scripts/directives/je-ace.js
+++ app/scripts/directives/je-ace.js
@@ -8,12 +8,12 @@ angular
       template: '<div ng-mouseenter="sync(false)" ng-mouseleave="sync(true)" class="je-ace" ui-ace="$parent.jsoneditor.ace.options" ng-model="$parent.jsoneditor.json"></div>',
       replace: true,
       transclude: true,
-      link: function($scope, iElement, iAttr) {
+      link: function(scope, element, attrs) {
 
         // We need to turn off the json sync when changing stuff
         // in the ace editor
-        $scope.sync = function(flag) {
-          $scope.jsoneditor.sync.json = flag;
+        scope.sync = function(flag) {
+          scope.jsoneditor.sync.json = flag;
         };
 
       }
